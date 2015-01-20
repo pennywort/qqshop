f465925fb1a97957c3afac80a17311a92294f792
pennywort <vitismo@gmail.com>
2015-01-21 00:23:03 +0700

    для админки

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from shop.models import Post
+from shop.models import CartItem
+from shop.models import Order
+from shop.models import spyon
+from shop.models import AcceptedOrder
 
 admin.site.register(Post)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(spyon)
+admin.site.register(AcceptedOrder)
