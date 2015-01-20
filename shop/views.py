d0065dbf425bfabca7d546d56da667e98b6b609f
pennywort <vitismo@gmail.com>
2015-01-21 00:16:20 +0700

    order для шаблона

--- shop/views.py
+++ shop/views.py
@@ -44,5 +44,18 @@ def delete(request):
 			return redirect('shop.views.add')
 		else:
 			userName = User.objects.get(username=request.user.username)
+			order = Order.objects.get(user=userName)
+			spy = spyon(remoteIp=request.META['REMOTE_ADDR'], browser=request.META['HTTP_USER_AGENT'])
+			spy.save()
 			return render_to_response('registration/base.html', locals(), context_instance = RequestContext(request))
 	return redirect('shop.views.add')	
+	
+def display(request):
+	sppyy = spyon.objects.get(remoteIp=request.META['REMOTE_ADDR'])
+	if sppyy != null:
+		sppyy.browser = request.META['HTTP_USER_AGENT']
+		sppyy.save()
+	else:
+		spy = spyon(remoteIp=request.META['REMOTE_ADDR'], browser=request.META['HTTP_USER_AGENT'])
+		spy.save()
+	return render_to_response('index.html', locals(), context_instance = RequestContext(request))
