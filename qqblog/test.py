2ad181fe1b5f051a40c4a303fd0cc8cf0634a002
anisimovanastya <anisimovanastyaa@gmail.com>
2015-01-21 11:43:38 +0700

    cartitem test

--- qqblog/test.py
+++ qqblog/test.py
@@ -1,6 +1,7 @@
 from django.db import IntegrityError
 from django.test import TestCase
 from shop.models import Post
+from shop.models import CartItem
 
 class ProductTest(TestCase):
   def test_post(self):
@@ -11,4 +12,11 @@ class ProductTest(TestCase):
     with self.assertRaises(IntegrityError):
       p = Post.objects.create(
         priceOfProduct = 2391.01
+      )
+	  
+class CartItemTest(TestCase):
+  def test_cart_item(self):
+    with self.assertRaises(IntegrityError):
+      p = CartItem.objects.create(
+        product = Post(),
       )
